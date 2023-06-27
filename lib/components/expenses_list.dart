739c9a80229bc4bc55f9c4d1c291a2ebfecca637
Eliana Ferreira <ebferreiran@gmail.com>
2023-06-27 18:45:59 -0400

    added remove expenses functionality

--- lib/components/expenses_list.dart
+++ lib/components/expenses_list.dart
@@ -3,13 +3,23 @@ import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: expenses.length,
-        itemBuilder: (ctx, index) => ExpenseItem(expense: expenses[index]));
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpenseItem(expense: expenses[index]),
+      ),
+    );
   }
 }
--- lib/screens/expenses.dart
+++ lib/screens/expenses.dart
@@ -44,6 +44,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,7 +64,8 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('Chart placeholder'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(
+                expenses: _registeredExpenses, onRemoveExpense: _removeExpense),
           ),
         ],
       ),
