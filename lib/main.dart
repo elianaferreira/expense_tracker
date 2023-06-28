847013c00c15f90b7b20003f80f013ad93986588
Eliana Ferreira <ebferreiran@gmail.com>
2023-06-27 20:00:43 -0400

    added dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,30 @@ import 'package:expense_tracker/screens/expenses.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: const Color(0XFF8F43EE));
+var kDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark, seedColor: const Color(0XFFF0EB8D));
 
 void main() {
   runApp(MaterialApp(
+    darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kDarkColorScheme.primary,
+          foregroundColor: kDarkColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: Dimens.padding,
+            vertical: Dimens.paddingS,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer),
+        )),
     theme: ThemeData().copyWith(
       useMaterial3: true,
       colorScheme: kColorScheme,
@@ -26,6 +47,7 @@ void main() {
         ),
       ),
     ),
+    themeMode: ThemeMode.system, //default
     home: const Expenses(),
   ));
 }
